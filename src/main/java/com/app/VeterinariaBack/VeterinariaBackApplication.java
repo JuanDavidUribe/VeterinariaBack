82d81f33d7dc87e2ee246af5db39f6f8a33451ae
Juan David Uribe <juan_uribe82201@elpoli.edu.co>
2022-12-17 01:52:37 -0500

    primer commit

---
+++ src/main/java/com/app/VeterinariaBack/VeterinariaBackApplication.java
@@ -0,0 +1,13 @@
+package com.app.VeterinariaBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/VeterinariaBack/VeterinariaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.VeterinariaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
